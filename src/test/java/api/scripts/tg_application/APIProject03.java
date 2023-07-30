be18f173ffe26c90c606343c6cd4fccb5831321e
Anatoli Kamyshev <Akamyshev93@gmail.com>
2023-07-30 03:07:30 -0500

    first task of the project 3 added

--- src/test/java/api/scripts/tg_application/APIProject03.java
+++ src/test/java/api/scripts/tg_application/APIProject03.java
@@ -8,14 +8,18 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utils.ConfigReader;
 import utils.DBUtil;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class APIProject03 {
@@ -44,35 +48,40 @@ public class APIProject03 {
          * - Validate first name, last name, email, and dob you sent in the request body is reflected on the
          * Database.
          */
-        /* AddAUser createAStudent = AddAUser.builder()
+        AddAUser createAStudent = AddAUser.builder()
                 .firstName("Anatoli").lastName("Kamyshev").email("anatoli.kamyshev@gmail.com").dob("1995-01-15")
                 .build();
-        int newStudent_id = response.jsonPath().getInt("id"); */
-        Connection conn = DBUtil.createDBConnection();
-        Statement stmt = conn.createStatement();
-        String sqlQuery = "SELECT * FROM students WHERE id = 0";
-        List<List<Object>> SQLResult = DBUtil.getQueryResultList(sqlQuery);
-        for (List<Object> row : SQLResult) {
-            String firstNameSQL = row.get(0).toString();
-            String lastNameSQL = row.get(1).toString();
-            String emailSQL = row.get(2).toString();
-            String dobSQL = row.get(3).toString();
-        }
-        System.out.println(SQLResult);
 
 
-        /* response = RestAssured.given()
+
+        response = RestAssured.given()
                 .spec(baseSpec)
                 .body(createAStudent)
                 .when().post("/students")
                 .then().log().body()
                 .assertThat().statusCode(200).time(Matchers.lessThan(2000L))
-                .body("firstName", equalTo(String.valueOf((DBUtil.getColumnNames(sqlQuery).get(0)))).body("lastName", equalTo(DBUtil.getColumnNames(sqlQuery).get(1))).body("email", equalTo(DBUtil.getColumnNames(sqlQuery).get(2))).body("dob", equalTo(DBUtil.getColumnNames(sqlQuery).get(3)))
-                .extract().response(); */
+                .extract().response();
 
-
-
-
+        int newStudent_id = response.jsonPath().getInt("id");
+        String sqlQuery = "SELECT * FROM student WHERE id = " + newStudent_id;
+        List<List<Object>> SQLResultList = DBUtil.getQueryResultList(sqlQuery);
+        List<Object> singleRowResult = SQLResultList.get(0); //this is the only one result with a unique ID after we created a query
+        System.out.println(singleRowResult);
+        /**
+         * Getting a BigDecimal and start casting it into an int
+         */
+        BigDecimal dbNewStudentId = (BigDecimal) singleRowResult.get(0); // I'm taking a value from the first row of the database which is "id" as a big decimal number here
+        int dbNewStudentIdInt = dbNewStudentId.intValue(); //casting from big decimal into an int value
+        List<Object> formattedDBResult = new ArrayList<>(singleRowResult);
+        formattedDBResult.set(0, dbNewStudentIdInt); //here I'm setting a casted int value at the index of 0 which is value of "id" coulumn
+        Assert.assertEquals(formattedDBResult, Arrays.asList(newStudent_id, createAStudent.getDob(), createAStudent.getEmail(), createAStudent.getFirstName(), createAStudent.getLastName()));
+        //delete user
+        response = RestAssured.given()
+                .spec(baseSpec)
+                .when().delete("/students/" + newStudent_id)
+                .then().log().all()
+                .assertThat().statusCode(200)
+                .extract().response();
 
     }
 
